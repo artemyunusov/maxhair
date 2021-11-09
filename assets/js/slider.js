684d7748cb2925fd23fb95dad445d41e1446980b
artemyunusov <artemyunusov@gmail.com>
2021-11-09 16:08:38 +0500

    first commit

---
+++ assets/js/slider.js
@@ -0,0 +1,52 @@
+$('.product-slider').slick({
+  slidesToShow: 4,
+  arrows: false,
+  dots: false,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
+
+$('.review-slider').slick({
+  slidesToShow: 3,
+  arrows: false,
+  dots: false,
+  centerMode: true,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
